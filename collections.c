e0aa513067e7d270c12ddba78dd9052cec07818e
ilonic23 <ilia.pwl@gmail.com>
2025-12-18 20:51:26 +0200

    Add singly linked list implementation

--- collections.c
+++ collections.c
@@ -1,7 +1,139 @@
 #include "collections.h"
+#include "stdlib.h"
 
-#include <stdio.h>
+// Singly linked list implementation
 
-void hello(void) {
-    printf("Hello, World!\n");
+linked_list_t* linked_list_init(void) {
+    linked_list_t *list = malloc(sizeof(linked_list_t));
+    if (!list) return NULL;
+    list->head = list->tail = NULL;
+    list->length = 0;
+    return list;
 }
+
+linked_list_node_t* linked_list_node_init(void* value) {
+    linked_list_node_t *node = malloc(sizeof(linked_list_node_t));
+    if (!node) return NULL;
+    node->value = value;
+    node->next = NULL;
+    return node;
+}
+
+void linked_list_node_free(linked_list_node_t* node) {
+    free(node);
+}
+
+void linked_list_append(linked_list_t* list, linked_list_node_t* node) {
+    if (!list || !node) return;
+
+    if (!list->head) // If the head is null
+    {
+        list->head = node;
+        list->tail = node; // The tail shouldn't have the value at this point.
+    }
+    else
+    {
+        list->tail->next = node;
+        list->tail = node;
+    }
+
+    list->length++;
+}
+
+linked_list_node_t* linked_list_node_at(linked_list_t* list, size_t index) {
+    if (!list) return NULL;
+    if (index >= list->length && index != 0) return NULL; // Allow 0 to be index here
+    linked_list_node_t *node = list->head;
+    for (int i = 0; i < index; i++)
+    {
+        if (!node || !node->next) return NULL;
+        node = node->next;
+    }
+    return node;
+}
+
+void* linked_list_at(linked_list_t* list, size_t index) {
+    linked_list_node_t *node = linked_list_node_at(list, index);
+    if (!node) return NULL;
+    return node->value;
+}
+
+void linked_list_remove(linked_list_t* list, size_t index) {
+    if (!list) return;
+    if (index >= list->length) return;
+
+    if (!index) // Remove first node
+    {
+        linked_list_node_t *to_free = list->head;
+        if (!to_free) return;
+
+        list->head = to_free->next;
+        if (!list->head) {
+            list->tail = NULL;
+        }
+        linked_list_node_free(to_free);
+    }
+    else
+    {
+        linked_list_node_t *left = linked_list_node_at(list, index - 1);
+        linked_list_node_t *node = linked_list_node_at(list, index);
+
+        if (!left || !node) return;
+
+        left->next = node->next;
+        if (!node->next) { // Removing last node
+            list->tail = left;
+        }
+        linked_list_node_free(node);
+    }
+
+    list->length--;
+}
+
+void linked_list_insert_at(linked_list_t* list, size_t index, linked_list_node_t* node) {
+    if (!list || !node) return;
+    if (index > list->length) return; // Allow insertion at end
+
+    if (!index) // Insert at beginning
+    {
+        node->next = list->head;
+        list->head = node;
+        if (!node->next) { // List was empty
+            list->tail = node;
+        }
+    }
+    else
+    {
+        linked_list_node_t *left = linked_list_node_at(list, index - 1);
+        if (!left) return;
+
+        node->next = left->next;
+        left->next = node;
+
+        if (!node->next) { // Inserted at end
+            list->tail = node;
+        }
+    }
+    list->length++;
+}
+
+void linked_list_set_at(linked_list_t* list, size_t index, void* value) {
+    if (!list) return;
+    if (index >= list->length && index != 0) return;
+    linked_list_node_t *node = linked_list_node_at(list, index);
+    if (!node) return;
+    node->value = value;
+}
+
+void linked_list_clear(linked_list_t* list) {
+    for (size_t i = 0; i < list->length; i++)
+        linked_list_remove(list, i);
+    list->length = 0;
+}
+
+void linked_list_free(linked_list_t* list) {
+    linked_list_clear(list);
+    free(list);
+}
+
+// Singly linked list implementation
--- collections.h
+++ collections.h
@@ -1,6 +1,103 @@
 #ifndef COLLECTIONS_H
 #define COLLECTIONS_H
+#include <stddef.h>
 
-void hello(void);
+// Singly linked list implementation
+
+typedef struct LinkedListNode
+{
+    void *value;
+    struct LinkedListNode *next;
+} linked_list_node_t;
+
+typedef struct
+{
+    size_t length;
+    linked_list_node_t *head;
+    linked_list_node_t *tail;
+} linked_list_t;
+
+/**
+ * @brief Initializes the linked list.
+ * @return Initialized linked list pointer. NULL if failed.
+ */
+linked_list_t *linked_list_init(void);
+
+/**
+ * @brief *Initializes a linked list node.
+ * @return Initialized node pointer. NULL if failed.
+ */
+linked_list_node_t *linked_list_node_init(void *value);
+
+/**
+ * Frees the memory of the node.
+ * @param node The node to free the memory of.
+ */
+void linked_list_node_free(linked_list_node_t *node);
+
+/**
+ * @brief Append a node to the tail.
+ * @param list The list to append the node to.
+ * @param node The node to append to.
+ */
+void linked_list_append(linked_list_t *list, linked_list_node_t *node);
+
+/**
+ * @brief Get a node from the list at a specific index.
+ * @param list The list to get the node from.
+ * @param index The index to get the node at.
+ * @return Pointer to the node. NULL if it doesn't exist.
+ */
+linked_list_node_t *linked_list_node_at(linked_list_t *list, size_t index);
+
+/**
+ * @brief Get a value from the list at a specific index.
+ * @param list The list to get the value from.
+ * @param index The index to get the value at.
+ * @return
+ */
+void *linked_list_at(linked_list_t *list, size_t index);
+
+/**
+ * @brief Remove a node from the list at a specific index.
+ * Nodes that were disconnected will be reconnected.
+ * The node's memory will be freed.
+ * @param list The list to remove the node from.
+ * @param index The index to remove the node at.
+ * If the index points farther than the tail nothing would happen.
+ */
+void linked_list_remove(linked_list_t *list, size_t index);
+
+/**
+ * @brief Inserts a node to the list at the index.
+ * @param list The list to insert the node to.
+ * @param index The index to insert the node at.
+ * If the index points farther than the tail the node wouldn't be inserted.
+ * @param node The node to insert.
+ */
+void linked_list_insert_at(linked_list_t *list, size_t index, linked_list_node_t *node);
+
+/**
+ * @brief
+ * @param list The list to change the value in.
+ * @param index The index to set the value at.
+ * If the index points farther than the tail nothing would happen.
+ * @param value The value to set.
+ */
+void linked_list_set_at(linked_list_t *list, size_t index, void *value);
+
+/**
+ * Clears the list of every node and frees memory of all the nodes.
+ * @param list The list to clear.
+ */
+void linked_list_clear(linked_list_t *list);
+
+/**
+ * Clears the list and then frees it's memory.
+ * @param list The list to free the memory of.
+ */
+void linked_list_free(linked_list_t *list);
+
+// Singly linked list implementation
 
 #endif
